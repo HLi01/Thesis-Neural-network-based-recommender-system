f67d729cf963eb898bb659cf57838d0bcff03f0c
HLi01 <hermanl@stud.uni-obuda.hu>
2023-05-16 20:51:38 +0200

    GUI stackedwidgets added

--- GUI_PyQt.py
+++ GUI_PyQt.py
@@ -1,9 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableView, QWidget, QFileDialog, QMessageBox , QLabel, QLineEdit, QPushButton, QTextEdit, QGridLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QProgressBar, QDialog, QTableView, QWidget, QFileDialog, QMessageBox , QLabel, QLineEdit, QPushButton, QTextEdit, QGridLayout
 from PyQt6.QtGui import QIcon
-from PyQt6.QtCore import Qt
-from PyQt6.QtCore import QSize
-from PyQt6 import uic
-import sys
+from PyQt6.QtCore import Qt, QSize, QBasicTimer
+from PyQt6 import uic, QtWidgets
+import sys, time
 import neural_network
 import pandas as pd
 import numpy as np
@@ -12,7 +11,7 @@ nn=neural_network.NeuralNetwork()
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super(MainWindow, self).__init__()
         uic.loadUi("./UI/MainMenu.ui", self)
         self.showMaximized()
         
@@ -44,11 +43,10 @@ class MainWindow(QMainWindow):
         pass
 
     def loadDataset(self):
-        self.loadWindow=LoadWindow()
-        self.loadWindow.show()
+        stackedWidget.setCurrentIndex(1)
         
     def help(self):
-        pass
+        stackedWidget.setCurrentIndex(3)
 
     def exit(self):
         pass
@@ -59,16 +57,35 @@ class LoadWindow(QMainWindow):
         uic.loadUi("./UI/LoadDatasetWindow.ui", self)
         self.showMaximized()
         self.app=app
-
+        self.progressBar.setVisible(False)
+        self.timer = QBasicTimer()
+        self.step = 100
         self.ButtonLoadDataset.clicked.connect(self.loadDataset)
         self.EditRatings.clicked.connect(self.editRatings)
         self.ButtonBack.clicked.connect(self.back)
+        
+    def run(self):
+        for i in range(self.step):
+            time.sleep(0.01)
+            self.progressBar.setValue(i+1)
+        if self.step==100:
+            self.progressBar.setVisible(False)
+
+    def doAction(self):
+        if self.timer.isActive():
+            self.timer.stop()
+            self.btn.setText('Start')
+        else:
+            self.timer.start(100, self)
+            self.btn.setText('Stop')
 
     def loadDataset(self):
         dialog=QFileDialog()
         dialogSuccessful=dialog.exec()
         self.selectedFile=dialog.selectedFiles()
         if(dialogSuccessful):
+            self.progressBar.setVisible(True)
+            self.run()
             print(type(self.selectedFile[0]))
             #df=pd.read_csv(self.selectedFile[0], sep='\t')
             nn.loadCsv(csv_path=self.selectedFile[0])
@@ -81,12 +98,14 @@ class LoadWindow(QMainWindow):
             self.labelRatings.setText(f'{ratings} ratings')
             self.EditRatings.setEnabled(True)
 
+
     def editRatings(self):
-        self.editWindow=EditRatingsWindow()
-        self.editWindow.show()
+        stackedWidget.setCurrentIndex(2)
 
     def back(self):
-        self.close()
+        stackedWidget.setCurrentIndex(0)
+
+    
 
 class EditRatingsWindow(QMainWindow):
     def __init__(self):
@@ -104,12 +123,34 @@ class EditRatingsWindow(QMainWindow):
     def like():
         pass
 
+class HelpWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("./UI/HelpWindow.ui", self)
+        self.showMaximized()
+        self.app=app
+
+        self.ButtonBack.clicked.connect(self.back)
+
+    def back(self):
+        stackedWidget.setCurrentIndex(0)
+
 app=QApplication(sys.argv)
 with open('styles.css', 'r') as file:
     app.setStyleSheet(file.read())
     
 mainWindow=MainWindow()
-mainWindow.show()
+loadWindow=LoadWindow()
+editRatingsWindow=EditRatingsWindow()
+helpWindow=HelpWindow()
+stackedWidget=QtWidgets.QStackedWidget()
+stackedWidget.addWidget(mainWindow)
+stackedWidget.addWidget(loadWindow)
+stackedWidget.addWidget(editRatingsWindow)
+stackedWidget.addWidget(helpWindow)
+
+stackedWidget.show()
+stackedWidget.showMaximized()
 
 sys.exit(app.exec())
 
